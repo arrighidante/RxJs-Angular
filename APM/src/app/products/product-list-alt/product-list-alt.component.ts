8fe9cddaafb21420e94446505be6f35be2c1c8d4
Dante Arrighi <dantearrighi@gmail.com>
2022-06-05 18:51:10 -0300

    Added observables, actions and subjects
    Added observables to get products and product categories.
    Added behavior subject to do actions when user perform select category action

---
+++ APM/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { catchError, EMPTY, Subscription } from 'rxjs';
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list-alt.component.html'
+})
+export class ProductListAltComponent {
+  pageTitle = 'Products';
+  errorMessage = '';
+  selectedProductId = 0;
+
+  products$ = this.productService.products$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    )
+  constructor(private productService: ProductService) { }
+
+
+
+  onSelected(productId: number): void {
+    console.log('Not yet implemented');
+  }
+}
