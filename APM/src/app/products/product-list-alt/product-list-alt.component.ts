6a93ee066a0fdb637d54ab47dfd92b15ff2bad5f
Dante Arrighi <dantearrighi@gmail.com>
2022-06-06 15:31:14 -0300

    Added action to select a product and show details

--- APM/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 
 import { catchError, EMPTY, Subscription } from 'rxjs';
 
@@ -7,25 +7,28 @@ import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-list',
-  templateUrl: './product-list-alt.component.html'
+  templateUrl: './product-list-alt.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductListAltComponent {
   pageTitle = 'Products';
   errorMessage = '';
-  selectedProductId = 0;
 
-  products$ = this.productService.products$
+  products$ = this.productService.productsWithCategory$
     .pipe(
       catchError(err => {
         this.errorMessage = err;
         return EMPTY;
       })
     )
+
+  selectedProduct$ = this.productService.selectedProduct$;
+
   constructor(private productService: ProductService) { }
 
 
 
   onSelected(productId: number): void {
-    console.log('Not yet implemented');
+    this.productService.selectedProductChanged(productId);
   }
 }
